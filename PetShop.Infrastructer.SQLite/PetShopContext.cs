21193ba6770f855beccd9c044ec2602413cea643
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2020-09-29 22:22:27 +0200

    adding SQLite

---
+++ PetShop.Infrastructer.SQLite/PetShopContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PetShop.Infrastructer.SQLite
+{
+    public class PetShopContext
+    {
+    }
+}
---
+++ PetShop.Infrastructer.SQLite/Repositories/PetShopRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Infrastructer.SQLite.Data.Repositories
+{
+    class PetShopRepository
+    {
+    }
+}
